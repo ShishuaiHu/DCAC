e9ab2324b83331362f37eb3ecd9f9bb5090c1eb1
sshu <sshu@mail.nwpu.edu.cn>
2021-09-10 20:39:50 +0800

    add: abstract

---
+++ page_files/head.js
@@ -0,0 +1,27 @@
+var scripts = document.getElementsByTagName('script');
+var myScript = scripts[scripts.length - 1];
+
+var queryString = myScript.src.replace(/^[^\?]+\??/, '');
+
+var params = parseQuery(queryString);
+
+function parseQuery(query) {
+    var Params = {};
+    if (!query) return Params; // return empty object
+    var Pairs = query.split(/[;&]/);
+    for (var i = 0; i < Pairs.length; i++) {
+        var KeyVal = Pairs[i].split('=');
+        if (!KeyVal || KeyVal.length != 2) continue;
+        var key = unescape(KeyVal[0]);
+        var val = unescape(KeyVal[1]);
+        val = val.replace(/\+/g, ' ');
+        Params[key] = val;
+    }
+    return Params;
+}
+
+document.write('\
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\
+    <meta name=viewport content="width=device-width, initial-scale=1">\
+    <link rel="shortcut icon" href="' + params["prefix"] + 'images/favicon.ico">\
+');
